8556cb655f93ebfa75f2b7ce48265f326b189160
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-09 15:29:48 -0300

    Exclusão de classes desnecessárias

--- src/io/github/heltonricardo/cm/excecao/ExplosaoException.java
+++
@@ -1,7 +0,0 @@
-package io.github.heltonricardo.cm.excecao;
-
-public class ExplosaoException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- src/io/github/heltonricardo/cm/excecao/SairException.java
+++
@@ -1,7 +0,0 @@
-package io.github.heltonricardo.cm.excecao;
-
-public class SairException extends RuntimeException {
-
-	private static final long serialVersionUID = 1;
-
-}
