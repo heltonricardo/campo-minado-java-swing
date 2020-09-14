64a48068fe52f961443683478902fe0c9091d727
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-14 14:52:30 -0300

    Update BotaoCampo.java

--- src/io/github/heltonricardo/cm/visao/BotaoCampo.java
+++ src/io/github/heltonricardo/cm/visao/BotaoCampo.java
@@ -1,6 +1,8 @@
 package io.github.heltonricardo.cm.visao;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -9,7 +11,7 @@ import io.github.heltonricardo.cm.modelo.CampoEvento;
 import io.github.heltonricardo.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton implements CampoObservador {
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener {
 
 	private Campo campo;
 	private final Color BG_PADRAO = new Color(184, 184, 184);
@@ -21,6 +23,8 @@ public class BotaoCampo extends JButton implements CampoObservador {
 		this.campo = campo;
 		setBackground(BG_PADRAO);
 		setBorder(BorderFactory.createBevelBorder(0));
+		
+		addMouseListener(this);
 		campo.adicionarObservador(this);
 	}
 
@@ -42,13 +46,30 @@ public class BotaoCampo extends JButton implements CampoObservador {
 	}
 
 	private void aplicarEstiloAbrir() {
-		// TODO Auto-generated method stub
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		
+		switch (campo.minasNaVizinhanca()) {
+			case 1:
+				setForeground(TEXTO_VERDE); break;
+			case 2:
+				setForeground(Color.BLUE); break;
+			case 3:
+				setForeground(Color.YELLOW); break;
+			case 4: case 5: case 6:
+				setForeground(Color.RED); break;
+			default:
+				setForeground(Color.PINK);
+		}
+		
+		String valor = !campo.vizinhancaSegura() ?
+				Integer.toString(campo.minasNaVizinhanca()) : null;
+					
+		setText(valor);
 	}
 	
 	private void aplicarEstiloMarcar() {
 		// TODO Auto-generated method stub
-		
 	}
 	
 	private void aplicarEstiloExplodir() {
@@ -60,5 +81,19 @@ public class BotaoCampo extends JButton implements CampoObservador {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	// Interface dos eventos do mouse:
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.getButton() == 1)
+			campo.abrir();
+		else
+			campo.alternarMarcacao();
+	}
+	
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
 }
