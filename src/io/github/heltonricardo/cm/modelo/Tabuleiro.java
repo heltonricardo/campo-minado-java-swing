71b7454ce4e84f69ec202c9fe970ae4db4d7a4d7
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-14 13:17:57 -0300

    Update Tabuleiro.java

--- src/io/github/heltonricardo/cm/modelo/Tabuleiro.java
+++ src/io/github/heltonricardo/cm/modelo/Tabuleiro.java
@@ -7,9 +7,9 @@ import java.util.function.Consumer;
 
 public class Tabuleiro implements CampoObservador {
 
-	private int linhas;
-	private int colunas;
-	private int minas;
+	private final int linhas;
+	private final int colunas;
+	private final int minas;
 	
 	private final List<Campo> campos = new ArrayList<Campo>();
 	private final List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
@@ -22,6 +22,18 @@ public class Tabuleiro implements CampoObservador {
 		gerarCampos();
 		associarVizinhos();
 		sortearMinas();
+	}
+	
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+	
+	public void paraCadaCampo(Consumer<Campo> funcao) {
+		campos.forEach(funcao);
 	}
 	
 	public void adicionarObservador(Consumer<ResultadoEvento> obs) {
