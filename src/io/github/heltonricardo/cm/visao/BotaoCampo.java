64711e39bb67a8065c8299805a34cd7d41d05ff9
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-14 13:18:01 -0300

    Create BotaoCampo.java

---
+++ src/io/github/heltonricardo/cm/visao/BotaoCampo.java
@@ -0,0 +1,11 @@
+package io.github.heltonricardo.cm.visao;
+
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton{
+
+	public BotaoCampo() {
+		
+	}
+}
