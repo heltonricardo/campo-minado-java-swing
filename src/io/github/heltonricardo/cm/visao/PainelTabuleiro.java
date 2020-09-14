daf4d8af677626b6d02df7ec339eed171f97edda
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-14 12:55:34 -0300

    Create PainelTabuleiro.java

---
+++ src/io/github/heltonricardo/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package io.github.heltonricardo.cm.visao;
+
+import javax.swing.JPanel;
+
+import io.github.heltonricardo.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+	}
+}
