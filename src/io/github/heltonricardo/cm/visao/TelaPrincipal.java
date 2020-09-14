8ff00ff4e22fca8e2ed82a0842063b0073fb413c
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-14 12:55:37 -0300

    Update TelaPrincipal.java

--- src/io/github/heltonricardo/cm/visao/TelaPrincipal.java
+++ src/io/github/heltonricardo/cm/visao/TelaPrincipal.java
@@ -1,5 +1,28 @@
 package io.github.heltonricardo.cm.visao;
 
-public class TelaPrincipal {
+import javax.swing.JFrame;
 
+import io.github.heltonricardo.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame {
+
+	public TelaPrincipal() {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		
+		add(new PainelTabuleiro(tabuleiro));
+		
+		
+		setTitle("Campo Minado");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		new TelaPrincipal();
+	}
 }
